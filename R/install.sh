1658ea6c50425b14aee378f861144efdb5903855
Michael van Vliet <info@michaelvanvliet.nl>
2012-06-04 14:57:14 +0200

    Added additional packages to be installed to run Metitree

--- R/install.sh
+++ R/install.sh
@@ -2,4 +2,9 @@ R --slave <<EOF
 source("http://bioconductor.org/biocLite.R")
 biocLite("xcms")
 biocLite("Rgraphviz")
+biocLite("brew")
+biocLite("RCurl")
+biocLite("grid")
+biocLite("bitops")
+biocLite("graph")
 EOF
