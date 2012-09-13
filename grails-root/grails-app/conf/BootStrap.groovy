9941e3e446c2bb907ef8ee192ce91148ebcf851d
Michael van Vliet <info@michaelvanvliet.nl>
2012-09-13 10:15:55 +0200

    Added placeholder user group and user

--- grails-root/grails-app/conf/BootStrap.groovy
+++ grails-root/grails-app/conf/BootStrap.groovy
@@ -15,11 +15,17 @@ class BootStrap {
 
 			// DEMO ###################
 			// WITH LIMITED ACCESS
-			def demoUserGroup =	new Usergroup(name: "Demo Usergroup", website: "http://www.demo.com", address: "").save(flush: true)
+			def demoUserGroup = new Usergroup(name: "Demo Usergroup", website: "http://www.demo.com", address: "").save(flush: true)
 			def demo = new Member(usergroup: demoUserGroup, name:"Mr. D. Emo", username:"demo", email:"user@demo.com", password:"demo".encodeAsMD5(), admin: false).save()
 			def anonymous = new Member(usergroup: demoUserGroup, name:"anonymous", username:"anonymous", email:"anonymous@metitree.nl", password:"anonymous".encodeAsMD5(), admin: false).save()
 			//def admin = new Member(usergroup: demoUserGroup, name:"Mrs. A. min", username:"admin", email:"admin@demo.com", password:"admin".encodeAsMD5(), admin: true).save()
 		}
+
+		if (!Usergroup.findByName("usrgrp")){
+			//def usrgrp = new Usergroup(name: "usrgrp", website: "http://www.metitree.nl", address: "").save(flush: true)
+			//def usr = new Member(usergroup: usergroup, name:"usr", username:"usrnm", email:"usr@metitree.nl", password:"usrpsswrd".encodeAsMD5(), admin: false).save()
+		}
+
 		/* ----------------------------------------------------------------------------------------- */
 	}
 
