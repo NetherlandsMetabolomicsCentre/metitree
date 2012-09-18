0b103a996fc20fd42fc42866d14c5036a18567eb
Michael van Vliet <info@michaelvanvliet.nl>
2012-09-18 12:50:20 +0200

    Added a loop to update the lastStatus of a ProcessJob

--- grails-root/grails-app/jobs/nl/nmc/metitree/QueueRunnerJob.groovy
+++ grails-root/grails-app/jobs/nl/nmc/metitree/QueueRunnerJob.groovy
@@ -1,81 +1,75 @@
 package nl.nmc.metitree
 
 class QueueRunnerJob {
-	
+
 	def sessionRequired = true
 	def concurrent = true
-		
+
 	static triggers = {
 		simple name: 'JobRunner', startDelay: 1000, repeatInterval: 10000 //delay 1sec, run every 10sec
 	}
-	
+
 	def mconfService
 	def processService
-	
+
     def execute() {
+
+    		//make sure the status is in sync!
+    		ProcessJobQueue.list().each { j ->
+    			if (j.processjob.lastStatus  != j.status) {
+    		    		j.processjob.lastStatus = j.status
+    				j.processjob.save(flush: true)
+    			}
+    		}
+
 		//loop over all running jobs to see if the shoud time-out
 		ProcessJobQueue.findAllByStatus(1).each { runningJobInQueue ->
 			def jobDuration = ((new Date().time - runningJobInQueue.processjob.lastUpdated.time)/1000) as int
-			def maxJobDuration = mconfService.fromLabel("metitree.processing.jobs.max.execution") as int 
+			def maxJobDuration = mconfService.fromLabel("metitree.processing.jobs.max.execution") as int
 			if (jobDuration > maxJobDuration){
 				log.info "Job ${runningJobInQueue} is running longer than allowed (${jobDuration} seconds)."
 			}
 		}
-		
-				
-		//find out if there are jobs queued	
-		if (ProcessJobQueue.findAllByStatus(0).size() > 0){	
 
-			//find out if the max no. of jobs is already runnig		
-			if (ProcessJobQueue.findAllByStatus(1).size() < (mconfService.fromLabel("metitree.processing.jobs.parallel.max") as int)){			
-				
+
+		//find out if there are jobs queued
+		if (ProcessJobQueue.findAllByStatus(0).size() > 0){
+
+			//find out if the max no. of jobs is already runnig
+			if (ProcessJobQueue.findAllByStatus(1).size() < (mconfService.fromLabel("metitree.processing.jobs.parallel.max") as int)){
+
 				//fetch the next job to run
 				def processJobQueue = ProcessJobQueue.findByStatus(0, [max:1, sort:"dateCreated", order:"asc"] )
-				
-				if (processJobQueue){								
+
+				if (processJobQueue){
 
 					// fetch job to process
 					def job = ProcessJob.get(processJobQueue.processjob.id)
-					
+
 					if (job.active){
-					
+
 						// change status to running to make sure we only run it once
 						processJobQueue.status = 1 // set to running
 						processJobQueue.save(flush: true) // force a save
-						
+
 						try {
-							job.report = ""
-							job.lastStatus = 1 // save status to job						
-							job.save(flush: true)
-							
-							processService.runJob(job)
-							
+							processService.runJob(job) // RUN !!!
 							processJobQueue.status = 2 // set to finished
 							processJobQueue.save() // force a save
-							
-							job.lastStatus = 2 // save status to job
-							job.save(flush: true)
 						} catch (e){
-							log.error("Was unable to execute job (${job})" + e)
 							processJobQueue.status = 99 // set to error
 							processJobQueue.save(flush: true) // force a save
-							
-							job.lastStatus = 99 // save status to job
-							job.save(flush: true)
+							log.error("Was unable to execute job (${job})" + e)
 						}
 					} else {
-					
+
 						// job inactive
 						processJobQueue.status = 11 // set to running
 						processJobQueue.save(flush: true) // force a save
-						
-						job.lastStatus = 11 // save status to job
-						job.save(flush: true)
-					
 						log.info "Job is in-active, will not process."
 					}
 				}
-			} 
+			}
 		}
 
 		//log.info("Queue runners at ${ProcessJobQueue.findAllByStatus(1).size()} of max ${mconfService.fromLabel('metitree.processing.jobs.parallel.max')} with ${mconfService.fromLabel('metitree.processing.jobs.threads.max')} thread(s) per runner.")
